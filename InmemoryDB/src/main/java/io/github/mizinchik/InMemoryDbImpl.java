8b29dbf6452af743ed8100ab73303e0fdf46d8fb
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2023-05-31 23:22:07 +0700

    Require not null, add javadoc

--- InmemoryDB/src/main/java/io/github/mizinchik/InMemoryDbImpl.java
+++ InmemoryDB/src/main/java/io/github/mizinchik/InMemoryDbImpl.java
@@ -1,21 +1,38 @@
 package io.github.mizinchik;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+/**
+ * InMemoryDb with the lowest memory usage and retrieving
+ * in log(n) time.
+ *
+ * @author MIZINCHIK
+ */
 public class InMemoryDbImpl implements InMemoryDb {
     private final Map<Long, Set<Person>> accountToPerson;
     private final Map<String, Set<Person>> nameToPerson;
     private final Map<Double, Set<Person>> valueToPerson;
 
+    /**
+     * Constructs an empty db.
+     */
     public InMemoryDbImpl() {
         accountToPerson = new TreeMap<>();
         nameToPerson = new TreeMap<>();
         valueToPerson = new TreeMap<>();
     }
 
+    /**
+     * @inheritDoc
+     * Sets are TreeSets for less memory usage.
+     * Entry is stored in 3 sets in 3 respective maps.
+     *
+     * @param person to add to the db
+     */
     @Override
     public void addEntry(Person person) {
         Long account = person.getAccount();
@@ -62,7 +79,7 @@ public class InMemoryDbImpl implements InMemoryDb {
     @Override
     public void changeEntry(Person person, Person newPerson) {
         if (deleteEntry(person)) {
-            addEntry(newPerson);
+            addEntry(Objects.requireNonNull(newPerson));
         }
     }
 
--- InmemoryDB/src/main/java/io/github/mizinchik/Person.java
+++ InmemoryDB/src/main/java/io/github/mizinchik/Person.java
@@ -2,39 +2,85 @@ package io.github.mizinchik;
 
 import java.util.Objects;
 
+/**
+ * Entry class for a simple database.
+ * Represents a client of ATON.
+ *
+ * @author MIZINCHIK
+ */
 public class Person implements Comparable<Person> {
     private Long account;
     private String name;
     private Double value;
 
+    /**
+     * Constructs a client instance.
+     * Arguments mustn't be null.
+     *
+     * @param account of the client
+     * @param name of the client
+     * @param value of the client
+     */
     public Person(Long account, String name, Double value) {
-        this.account = account;
-        this.name = name;
-        this.value = value;
+        this.account = Objects.requireNonNull(account);
+        this.name = Objects.requireNonNull(name);
+        this.value = Objects.requireNonNull(value);
     }
 
+    /**
+     * Returns the client's account.
+     *
+     * @return account
+     */
     public long getAccount() {
         return account;
     }
 
+    /**
+     * Returns the client's name.
+     *
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the client's account value.
+     *
+     * @return value
+     */
     public double getValue() {
         return value;
     }
 
-    public void setAccount(long account) {
-        this.account = account;
+    /**
+     * Changes the client's account.
+     *
+     * @param account to set
+     */
+    public void setAccount(Long account) {
+        this.account = Objects.requireNonNull(account);
     }
 
+
+    /**
+     * Changes the client's name.
+     *
+     * @param name to set
+     */
     public void setName(String name) {
-        this.name = name;
+        this.name = Objects.requireNonNull(name);
     }
 
-    public void setValue(double value) {
-        this.value = value;
+
+    /**
+     * Changes the client's account value.
+     *
+     * @param value to set
+     */
+    public void setValue(Double value) {
+        this.value = Objects.requireNonNull(value);
     }
 
     @Override
@@ -44,21 +90,36 @@ public class Person implements Comparable<Person> {
 
         Person person = (Person) o;
 
-        if (!Objects.equals(account, person.account)) return false;
-        if (!Objects.equals(name, person.name)) return false;
-        return Objects.equals(value, person.value);
+        if (!account.equals(person.account)) return false;
+        if (!name.equals(person.name)) return false;
+        return value.equals(person.value);
     }
 
     @Override
     public int hashCode() {
-        int result = account != null ? account.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (value != null ? value.hashCode() : 0);
+        int result = account.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + value.hashCode();
         return result;
     }
 
+    /**
+     * Creates a natural ordering of the entries
+     * to put them into a TreeMap.
+     *
+     * @param o the object to be compared.
+     * @return 1 as > 0 as == -1 as <
+     */
     @Override
     public int compareTo(Person o) {
-        return getName().compareTo(o.getName());
+        int compare = Long.compare(getAccount(), o.getAccount());
+        if (compare != 0) {
+            return compare;
+        }
+        compare = getName().compareTo(o.getName());
+        if (compare != 0) {
+            return compare;
+        }
+        return Double.compare(getValue(), o.getValue());
     }
 }
